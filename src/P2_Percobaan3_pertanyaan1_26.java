285f3b47e5a260a4f6abe1bdaf3775407dac3f74
RobbyCaturW <you@example.comrobbycatur330@gmail.com>
2024-09-09 23:00:17 +0700

    data KRS

---
+++ src/P2_Percobaan3_pertanyaan1_26.java
@@ -0,0 +1,17 @@
+public class P2_Percobaan3_pertanyaan1_26 {
+    public static void main(String[] args) {
+        System.out.println("Tahun Akademik\t\t: 2024/2025 Ganjil");
+        System.out.println("Kelas\t\t\t: 1G");
+        System.out.println("IP Semester Lalu\t: 0.00");
+        System.out.println("IP Komulatif\t\t: 0.00\n");
+        System.out.println("NO\tKODE MK\t\tMATA KULIAH\t\t\t\tSEMESTER\tSKS\tJAM");
+        System.out.println("1\tRTI241001\tPancasila\t\t\t\t1\t\t2\t4");
+        System.out.println("2\tRTI241002\tKonsep Teknologi Informasi\t\t1\t\t2\t4");
+        System.out.println("3\tRTI241003\tCritical Thinking dan Problem Solving\t1\t\t2\t4");
+        System.out.println("4\tRTI241004\tMatematika Dasar\t\t\t1\t\t3\t6");
+        System.out.println("5\tRTI241005\tRekayasa Perangkat Lunak\t\t1\t\t2\t4");
+        System.out.println("6\tRTI241006\tDasar Pemrograman\t\t\t1\t\t2\t4");
+        System.out.println("7\tRTI241007\tPraktikum Dasar Pemrograman\t\t1\t\t3\t6");
+        System.out.println("8\tRTI241008\tKeselamatan dan Kesehatan Kerja\t\t1\t\t2\t4");
+    }
+}
