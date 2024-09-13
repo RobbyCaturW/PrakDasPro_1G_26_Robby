7d434421d6dab8a76ae41e54136a00f944001771
RobbyCaturW <you@example.comrobbycatur330@gmail.com>
2024-09-13 17:54:07 +0700

    P3_Percobaan2_PertanyaanNo3

--- src/Pertemuan3/Kafe26.java
+++ src/Pertemuan3/Kafe26.java
@@ -1,6 +1,7 @@
 package Pertemuan3;
 
 import java.util.Scanner;
+
 public class Kafe26 {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
@@ -18,8 +19,9 @@ public class Kafe26 {
         jmlRoti = input.nextInt();
         double totalHarga = (jmlKopi * hargaKopi) + (jmlTeh * hargaTeh) + (jmlRoti * hargaRoti);
         double nominalBayar = totalHarga - (diskon * totalHarga);
+        int nominalInt = (int) nominalBayar;
         System.out.println("Keanggotaan pelanggan " + keanggotaan);
         System.out.println("Item pembelian " + jmlKopi + " kopi, " + jmlTeh + " teh, " + jmlRoti + " roti");
-        System.out.println("Nominal Bayar Rp " + nominalBayar);
+        System.out.println("Nominal Bayar Rp " + nominalInt);
     }
 }
