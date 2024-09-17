2501caad270382b7b95abb8d116528fcba71393e
RobbyCaturW <you@example.comrobbycatur330@gmail.com>
2024-09-17 22:27:12 +0700

    Tugas

--- src/Pertemuan3/P3_Tugas2.java
+++ src/Pertemuan3/P3_Tugas2.java
@@ -9,19 +9,19 @@ public class P3_Tugas2 {
 
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        int jumlahJamKerja, upahPerJam, gajiKaryawan, gajiSebelumPajak, gajiBersih;
+        int jumlahJamKerja, upahPerJam;
+        double gajiKaryawan, bonusGaji, gajiSebelumPajak, gajiSetelahPajak, gajiBersih;
         System.out.print("Masukkan jumlah jam kerja karyawan: ");
         jumlahJamKerja = input.nextInt();
         System.out.print("Masukkan upah per jam: ");
         upahPerJam = input.nextInt();
-        float bonusGaji, gajiSetelahPajak;
         gajiKaryawan = jumlahJamKerja * upahPerJam;
         System.out.println("Gaji Karyawan: " + gajiKaryawan);
-        bonusGaji = gajiKaryawan * (10 / 100f);
+        bonusGaji = gajiKaryawan * 0.1;
         System.out.println("Bonus Gaji: " + bonusGaji);
-        gajiSebelumPajak = (int) (gajiKaryawan + bonusGaji);
+        gajiSebelumPajak = gajiKaryawan + bonusGaji;
         System.out.println("Gaji Sebelum Pajak: " + gajiSebelumPajak);
-        gajiSetelahPajak = bonusGaji * (5 / 100f);
+        gajiSetelahPajak = bonusGaji * 0.05;
         System.out.println("Gaji Setelah Pajak: " + gajiSetelahPajak);
         gajiBersih = (int) (gajiKaryawan - gajiSetelahPajak);
         System.out.println("Gaji bersih sebesar: " + gajiBersih);
