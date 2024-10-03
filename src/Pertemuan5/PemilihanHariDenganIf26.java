dc8a2ed39dc30e8b7fe906ab3b4de9db26ca8026
RobbyCaturW <you@example.comrobbycatur330@gmail.com>
2024-10-03 16:17:43 +0700

    P5_Percobaan3_PertanyaanNo4

---
+++ src/Pertemuan5/PemilihanHariDenganIf26.java
@@ -0,0 +1,23 @@
+package Pertemuan5;
+
+import java.util.Scanner;
+
+public class PemilihanHariDenganIf26 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int dayNumber;
+        String dayType;
+        System.out.print("Input day number: ");
+        dayNumber = sc.nextInt();
+        if (dayNumber >= 1 && dayNumber <= 5) {
+            dayType = "weekday";
+            System.out.println(dayNumber + " is a " + dayType); 
+       } else if (dayNumber >= 6 && dayNumber <= 7) {
+        dayType = "weekend";
+        System.out.println(dayNumber + " is a " + dayType);
+       } else if (dayNumber < 1 || dayNumber > 7) {
+        dayType = "invalid day number";
+        System.out.println(dayNumber + " is a " + dayType);
+       }
+    }
+}
