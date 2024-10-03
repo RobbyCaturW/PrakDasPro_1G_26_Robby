1d263b054a75cea2bd3c71a9e78e0bc5cf91d43d
RobbyCaturW <you@example.comrobbycatur330@gmail.com>
2024-10-03 09:14:45 +0700

    P5_Percobaan1_PertanyaanNo1

--- src/Pertemuan5/PemilihanBilangan26.java
+++ src/Pertemuan5/PemilihanBilangan26.java
@@ -8,14 +8,12 @@ import java.util.Scanner;
 public class PemilihanBilangan26 {
 
     public static void main(String[] args) {
+        
         Scanner sc = new Scanner(System.in);
-
+        
         System.out.print("Masukkan sebuah angka: ");
         int angka = sc.nextInt();
-        if (angka % 2 == 0) {
-            System.out.println("Angka " + angka + " termasuk bilangan genap");
-        } else {
-            System.out.println("Angka " + angka + " termasuk bilangan ganjil");
-        }
+        String hasil = (angka % 2 == 0) ? "Angka " + angka + " termasuk bilangan genap" : "Angka " + angka + " termasuk bilangan ganjil";
+        System.out.println((hasil));
     }
 }
