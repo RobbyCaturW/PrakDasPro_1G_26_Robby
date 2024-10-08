fc09fac87c90ef3bd21787b69ea05f2a3ce41ca1
RobbyCaturW <robbycatur330@gmail.com>
2024-10-08 12:11:39 +0700

    P6_Percobaan1_PertanyaanNo4

--- src/Pertemuan6/Pemilihan2Percobaan126.java
+++ src/Pertemuan6/Pemilihan2Percobaan126.java
@@ -8,9 +8,9 @@ public class Pemilihan2Percobaan126 {
         System.out.print("Masukkan Tahun: ");
         int tahun = input26.nextInt();
 
-        if ((tahun % 4) == 0) {
-            if ((tahun % 100) != 0) System.out.println("Tahun Kabisat");
-            else System.out.println("Bukan Tahun Kabisat");
-        } else System.out.println("Bukan Tahun Kabisat");
+        if ((tahun % 400) == 0) System.out.println(("Tahun Kabisat"));
+        else if ((tahun % 100) == 0) System.out.println("Bukan Tahun Kabisat");
+        else if ((tahun % 4) == 0) System.out.println("Tahun Kabisat");
+        else System.out.println("Bukan Tahun Kabisat");
     }
 }
