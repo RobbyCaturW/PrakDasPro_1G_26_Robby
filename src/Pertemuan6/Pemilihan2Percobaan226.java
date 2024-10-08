c272e02da8fc5efa0c5c44870ac581dc04face44
RobbyCaturW <robbycatur330@gmail.com>
2024-10-08 16:25:02 +0700

    P6_Percobaan2_PertanyaanNo4

--- src/Pertemuan6/Pemilihan2Percobaan226.java
+++ src/Pertemuan6/Pemilihan2Percobaan226.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 public class Pemilihan2Percobaan226 {
     public static void main(String[] args) {
         Scanner input26 = new Scanner(System.in);
-        String member;
+        String member, qris;
         int pilihan_menu, harga;
         double diskon, total_bayar;
         System.out.println("-------------------------");
@@ -20,6 +20,8 @@ public class Pemilihan2Percobaan226 {
         input26.nextLine();
         System.out.print("Apakah punya member (y/n) ? = ");
         member = input26.nextLine();
+        System.out.print("Apakah Anda ingin membayar menggunakan QRIS (y/n) ? = ");
+        qris = input26.nextLine();
         if (member.equalsIgnoreCase("y")) {
             diskon = 0.10;
             System.out.println("Besar diskon = 10%");
@@ -38,6 +40,10 @@ public class Pemilihan2Percobaan226 {
             }
             // Menghitung total bayar
             total_bayar = harga - (harga * diskon);
+            if (qris.equalsIgnoreCase("y")) {
+                total_bayar -= 1000;
+                System.out.println("Bonus diskon bayar menggunakan QRIS = Rp1.000");
+            }
             System.out.println("Total bayar setelah diskon = " + total_bayar);
         } else if (member.equalsIgnoreCase("n")) {
             if (pilihan_menu == 1) {
@@ -52,6 +58,10 @@ public class Pemilihan2Percobaan226 {
             } else {
                 System.out.println("Masukkan pilihan menu secara benar");
                 return;
+            }
+            if (qris.equalsIgnoreCase("y")) {
+                harga -= 1000;
+                System.out.println("Bonus diskon bayar menggunakan QRIS = Rp1.000");
             }
             // Menghitung total bayar
 
