245d0629b33cf195d052beddb264f6377a2ef856
RobbyCaturW <robbycatur330@gmail.com>
2024-10-11 15:03:12 +0700

    P7_Percobaan1_PertanyaanNo4

--- src/Pertemuan7/SiakadFor26.java
+++ src/Pertemuan7/SiakadFor26.java
@@ -10,6 +10,8 @@ public class SiakadFor26 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         double nilai, tertinggi = 0, terendah = 100;
+        boolean isLulus;
+        int lulus = 0, tidakLulus = 0;
         for (int i = 1; i <= 10; i++) {
             System.out.print("Masukkan nilai mahasiswa ke-" + i + ": ");
             nilai = sc.nextDouble();
@@ -19,8 +21,12 @@ public class SiakadFor26 {
             if (nilai < terendah) {
                 terendah = nilai;
             }
+            if (nilai  >= 60) lulus++;
+            else tidakLulus++;
         }
         System.out.println("Nilai tertinggi: " + tertinggi);
         System.out.println("Nilai terendah: " + terendah);
+        System.out.println("Jumlah mahasiswa lulus: " + lulus);
+        System.out.println("Jumlah mahasiswa tidak lulus: " + tidakLulus);
     }
 }
