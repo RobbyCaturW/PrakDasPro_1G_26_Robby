3d840ca15028b979e741ebb321e4b296294e1b33
RobbyCaturW <robbycatur330@gmail.com>
2024-11-01 15:15:28 +0700

    Revert "P10_Percobaan1_Pertanyaan2"
    This reverts commit 584077ad2f824103713221e53df5a350d47738bb.

--- src/Pertemuan9/Triangle26.java
+++ src/Pertemuan9/Triangle26.java
@@ -7,7 +7,7 @@ public class Triangle26 {
         Scanner sc = new Scanner(System.in);
         System.out.print("Masukkan nilai N = ");
         int N = sc.nextInt();
-        int i = 0;
+        int i = 1;
         while (i <= N) {
             int j = 0;
             while (j < i) {
