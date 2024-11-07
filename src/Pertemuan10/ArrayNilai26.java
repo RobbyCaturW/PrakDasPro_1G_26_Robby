b4e6e37203e748182d15ea45fab0aec3ed458b6e
RobbyCaturW <robbycatur330@gmail.com>
2024-11-07 20:42:29 +0700

    P10_Percobaan2_Pertanyaan4

--- src/Pertemuan10/ArrayNilai26.java
+++ src/Pertemuan10/ArrayNilai26.java
@@ -16,7 +16,7 @@ public class ArrayNilai26 {
         for (int i = 0; i < nilaiAkhir.length; i++) {
             if (nilaiAkhir[i] > 70) {
                 System.out.println("Mahasiswa ke-" + i + " lulus!");
-            }
+            } else System.out.println("Mahasiswa ke-" + i + " tidak lulus!");
         }
         sc.close();
     }
