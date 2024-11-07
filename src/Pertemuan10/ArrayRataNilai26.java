5eaec6cd2f4d5732d65c50760fc7848465dd9de5
RobbyCaturW <robbycatur330@gmail.com>
2024-11-07 21:02:36 +0700

    P10_Percobaan3_Pertanyaan1

--- src/Pertemuan10/ArrayRataNilai26.java
+++ src/Pertemuan10/ArrayRataNilai26.java
@@ -6,6 +6,7 @@ public class ArrayRataNilai26 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int[] nilaiMhs = new int[10];
+        int mhsLulus = 0;
         double total = 0, rata2;
         for (int i = 0; i < nilaiMhs.length; i++) {
             System.out.print("Masukkan nilai mahasiswa ke-" + (i+1) + " : ");
@@ -13,8 +14,13 @@ public class ArrayRataNilai26 {
         }
         for (int i = 0; i < nilaiMhs.length; i++) {
             total += nilaiMhs[i];
+            if (nilaiMhs[i] > 70) {
+                mhsLulus += 1;
+            }
         }
         rata2 = total/nilaiMhs.length;
         System.out.println("Rata-rata nilai = " + rata2);
+        System.out.println("Jumlah mahasiswa yang lulus = " + mhsLulus);
+        
     }
 }
