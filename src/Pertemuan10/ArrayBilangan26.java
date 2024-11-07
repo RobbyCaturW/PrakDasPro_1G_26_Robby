50e9a3d1a7f5e1c0e03de66579b3d92467044ee5
RobbyCaturW <robbycatur330@gmail.com>
2024-11-07 19:17:43 +0700

    P10_Percobaan1

--- src/Pertemuan10/ArrayBilangan26.java
+++ src/Pertemuan10/ArrayBilangan26.java
@@ -2,11 +2,16 @@ package Pertemuan10;
 
 public class ArrayBilangan26 {
     public static void main(String[] args) {
-        int[] bil = {5, 13, -7, 17};
+        int[] bil = new int[4];
 
-        for (int i = 0; i <= 4; i++) {
-            System.out.println(bil[i]);
-        }
+        bil[0] = 5;
+        bil[1] = 13;
+        bil[2] = -7;
+        bil[3] = 17;
 
+        System.out.println(bil[0]);
+        System.out.println(bil[1]);
+        System.out.println(bil[2]);
+        System.out.println(bil[3]);
     }
 }
