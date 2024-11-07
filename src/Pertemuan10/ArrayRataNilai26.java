b00204991b3ef864ad7b6ed56ff1a4cdc5d174a0
RobbyCaturW <robbycatur330@gmail.com>
2024-11-07 20:46:49 +0700

    P10_Percobaan3

--- src/Pertemuan10/ArrayRataNilai26.java
+++ src/Pertemuan10/ArrayRataNilai26.java
@@ -11,7 +11,7 @@ public class ArrayRataNilai26 {
             System.out.print("Masukkan nilai mahasiswa ke-" + (i+1) + " : ");
             nilaiMhs[i] = sc.nextInt();
         }
-        for (int i = 0; i < 10; i++) {
+        for (int i = 0; i < nilaiMhs.length; i++) {
             total += nilaiMhs[i];
         }
         rata2 = total/nilaiMhs.length;
