1293edd5e0959a29526fdc67321c097041be92f5
RobbyCaturW <robbycatur330@gmail.com>
2024-11-07 20:39:12 +0700

    P10_Percobaan2_Pertanyaan3

--- src/Pertemuan10/ArrayNilai26.java
+++ src/Pertemuan10/ArrayNilai26.java
@@ -11,9 +11,13 @@ public class ArrayNilai26 {
             System.out.print("Masukkan nilai akhir ke-" + i + ": ");
             nilaiAkhir[i] = sc.nextInt();
         }
-
-        for (int i = 0; i < 10; i++) {
-            System.out.println("Nilai akhir ke-" + i + " adalah " + nilaiAkhir[i]);
+        sc.close();
+        
+        for (int i = 0; i < nilaiAkhir.length; i++) {
+            if (nilaiAkhir[i] > 70) {
+                System.out.println("Mahasiswa ke-" + i + " lulus!");
+            }
         }
+        sc.close();
     }
 }
