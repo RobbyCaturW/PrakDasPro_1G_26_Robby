75573aa5dcca3564492d1b83ae67ec6e8a48f861
RobbyCaturW <robbycatur330@gmail.com>
2024-11-09 22:08:53 +0700

    P11_Percobaan1_Pertanyaan3

--- src/Pertemuan11/Bioskop26.java
+++ src/Pertemuan11/Bioskop26.java
@@ -11,6 +11,7 @@ public class Bioskop26 {
         penonton[2][0] = "Eka";
         penonton[2][1] = "Farhan";
         penonton[3][0] = "Gisel";
+        penonton[3][1] = "Gisel";
 
         System.out.printf("%s \t %s\n", penonton[0][0], penonton[0][1]);
         System.out.printf("%s \t %s\n", penonton[1][0], penonton[1][1]);
