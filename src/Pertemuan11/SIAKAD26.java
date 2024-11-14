82fe652617c1737401a3cc25c510049d74bb3971
RobbyCaturW <robbycatur330@gmail.com>
2024-11-14 22:34:23 +0700

    P11_Percobaan4_Pertanyaan1

--- src/Pertemuan11/SIAKAD26.java
+++ src/Pertemuan11/SIAKAD26.java
@@ -5,7 +5,12 @@ import java.util.Scanner;
 public class SIAKAD26 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int[][] nilai = new int[4][3];
+        int jmlMhs, jmlMatkul;
+        System.out.print("Masukkan jumlah mahasiswa: ");
+        jmlMhs = sc.nextInt();
+        System.out.print("Masukkan jumlah mata kuliah: ");
+        jmlMatkul = sc.nextInt();
+        int[][] nilai = new int[jmlMhs][jmlMatkul];
 
         for (int i = 0; i < nilai.length; i++) {
             System.out.println("Input nilai mahasiswa ke-" + (i+1));
@@ -17,7 +22,7 @@ public class SIAKAD26 {
                 totalPerSiswa += nilai[i][j];
             }
 
-            System.out.println("Nilai rata-rata: " + totalPerSiswa/3);
+            System.out.println("Nilai rata-rata: " + totalPerSiswa/jmlMatkul);
         }
 
         System.out.println("\n====================================");
@@ -28,7 +33,7 @@ public class SIAKAD26 {
             for (int i = 0; i < 4; i++) {
                 totalPerMatkul += nilai[i][j];
             }
-            System.out.println("Mata Kuliah " + (j + 1) + ": " + totalPerMatkul/4);
+            System.out.println("Mata Kuliah " + (j + 1) + ": " + totalPerMatkul/jmlMhs);
         }
     }
 }
