9b19a2f7afcd6b6985e3f5305d5fbb5d373e4284
RobbyCaturW <robbycatur330@gmail.com>
2024-11-29 13:39:55 +0700

    P13_Percobaan1_Pertanyaan2

--- src/Pertemuan13/Kafe26.java
+++ src/Pertemuan13/Kafe26.java
@@ -1,18 +1,16 @@
 package Pertemuan13;
 
 public class Kafe26 {
-    static void Menu() {
+    
+    public static void main(String[] args) {
         System.out.println("===== MENU RESTO KAFE =====");
         System.out.println("1. Kopi Hitam - Rp 15,000");
-        System.out.println("1. Cappuccino - Rp 20,000");
-        System.out.println("1. Latte - Rp 22,000");
-        System.out.println("1. Teh Tarik - Rp 12,000");
-        System.out.println("1. Roti Bakar - Rp 10,000");
-        System.out.println("1. Mie Goreng - Rp 18,000");
+        System.out.println("2. Cappuccino - Rp 20,000");
+        System.out.println("3. Latte - Rp 22,000");
+        System.out.println("4. Teh Tarik - Rp 12,000");
+        System.out.println("5. Roti Bakar - Rp 10,000");
+        System.out.println("6. Mie Goreng - Rp 18,000");
         System.out.println("===========================");
         System.out.println("Silakan pilih menu yang Anda inginkan.");
-    }
-    public static void main(String[] args) {
-        Menu();
     }
 }
