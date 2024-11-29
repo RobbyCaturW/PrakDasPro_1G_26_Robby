7e2e63c9db855594d95139d06ed06bc6c34af0c0
RobbyCaturW <robbycatur330@gmail.com>
2024-11-29 13:44:16 +0700

    P13_Percobaan2

--- src/Pertemuan13/Kafe26.java
+++ src/Pertemuan13/Kafe26.java
@@ -1,8 +1,13 @@
 package Pertemuan13;
 
 public class Kafe26 {
-    
-    public static void main(String[] args) {
+    static void Menu(String namaPelanggan, boolean isMember) {
+        System.out.println("Selamat datang, " + namaPelanggan + "!");
+
+        if (isMember) {
+            System.out.println("Anda adalah member, dapatkan diskon 10% untuk setiap pembelian!");
+        }
+
         System.out.println("===== MENU RESTO KAFE =====");
         System.out.println("1. Kopi Hitam - Rp 15,000");
         System.out.println("2. Cappuccino - Rp 20,000");
@@ -12,5 +17,8 @@ public class Kafe26 {
         System.out.println("6. Mie Goreng - Rp 18,000");
         System.out.println("===========================");
         System.out.println("Silakan pilih menu yang Anda inginkan.");
+    }
+    public static void main(String[] args) {
+        Menu("Andi", true);
     }
 }
