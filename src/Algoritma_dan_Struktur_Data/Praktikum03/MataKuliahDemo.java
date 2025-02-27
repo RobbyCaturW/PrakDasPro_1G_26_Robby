766366b8c919ac9c664caf9667f390e2d78e2e22
RobbyCaturW <robbycatur330@gmail.com>
2025-02-27 10:31:28 +0700

    Percobaan3_Pertanyaan4

--- src/Algoritma_dan_Struktur_Data/Praktikum03/MataKuliahDemo.java
+++ src/Algoritma_dan_Struktur_Data/Praktikum03/MataKuliahDemo.java
@@ -5,15 +5,17 @@ import java.util.Scanner;
 public class MataKuliahDemo {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        MataKuliah25[] arrayOfMataKuliah = new MataKuliah25[3];
+        System.out.print("Masukkan jumlah mata kuliah: ");
+        int n = sc.nextInt();
+        MataKuliah25[] arrayOfMataKuliah = new MataKuliah25[n];
 
-        for (int i = 0; i < 3; i++) {
+        for (int i = 0; i < n; i++) {
             System.out.println("Masukkan Data Mata Kuliah ke-" + (i + 1));
             arrayOfMataKuliah[i] = new MataKuliah25("", "", 0, 0);
             arrayOfMataKuliah[i].tambahData(sc);
         }
 
-        for (int i = 0; i < 3; i++) {
+        for (int i = 0; i < n; i++) {
             System.out.println("Data Mata Kuliah ke-" + (i+1));
             arrayOfMataKuliah[i].cetakInfo();
             System.out.println("-----------------------------");
