9080f0358cd9df26f4fa419d822e7c8eebccf74f
RobbyCaturW <robbycatur330@gmail.com>
2025-05-22 10:29:53 +0700

    Praktikum 3

--- src/Algoritma_dan_Struktur_Data/doublelinkedlist/DoubleLinkedListsMain.java
+++ src/Algoritma_dan_Struktur_Data/doublelinkedlist/DoubleLinkedListsMain.java
@@ -39,5 +39,22 @@ public class DoubleLinkedListsMain {
         dll.print();
         System.out.println("Size: " + dll.size());
         System.out.println("============================================");
+        dll.removeFirst();
+        dll.print();
+        System.out.println("Size: " + dll.size());
+        System.out.println("============================================");
+        dll.addFirst(3);
+        dll.addLast(4);
+        dll.addFirst(7);
+        dll.print();
+        System.out.println("Size: " + dll.size());
+        System.out.println("============================================");
+        dll.add(40, 1);
+        dll.print();
+        System.out.println("Size: " + dll.size());
+        System.out.println("============================================");
+        System.out.println("Data awal pada Linked Lists adalah: " + dll.getFirst());
+        System.out.println("Data akhir pada Linked Lists adalah: " + dll.getLast());
+        System.out.println("Data indeks ke-1 pada Linked Lists adalah: " + dll.get(1));
     }
 }
