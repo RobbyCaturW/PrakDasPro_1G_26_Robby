34be72f8b8d6a9114185f466eb6fcc7e58887024
RobbyCaturW <robbycatur330@gmail.com>
2025-05-22 08:45:47 +0700

    Praktikum 1

---
+++ src/Algoritma_dan_Struktur_Data/doublelinkedlist/DoubleLinkedLists.java
@@ -0,0 +1,89 @@
+package Algoritma_dan_Struktur_Data.doublelinkedlist;
+
+public class DoubleLinkedLists {
+    Node head;
+    int size;
+
+    public DoubleLinkedLists() {
+        head = null;
+        size = 0;
+    }
+
+    public boolean isEmpty() {
+        return head == null;
+    }
+
+    public void addFirst(int item) {
+        if (isEmpty()) {
+            head = new Node(item, null, head);
+        } else {
+            Node newNode = new Node(item, null, head);
+            head.prev = newNode;
+            head = newNode;
+        }
+        size++;
+    }
+
+    public void addLast (int item) {
+        if (isEmpty()) {
+            addFirst(item);
+        } else {
+            Node current = head;
+            while (current.next != null) { 
+                current = current.next;
+            }
+            Node newNode = new Node(item, current, null);
+            current.next = newNode;
+            size++;
+        }
+    }
+
+    public void add(int item, int index) throws Exception {
+        if (isEmpty()) {
+            addFirst(item);
+        } else if (index < 0 || index > size) {
+            throw new Exception("Nilai indeks di luar batas");
+        } else {
+            Node current = head;
+            int i = 0;
+            while (i < index) { 
+                current = current.next;
+                i++;
+            }
+            if (current.prev == null) {
+                Node newNode = new Node(item, null, current);
+                current.prev = newNode;
+                head = newNode;
+            } else {
+                Node newNode = new Node(item, current.prev, current);
+                newNode.prev = current.prev;
+                newNode.next = current;
+                current.prev.next = newNode;
+                current.prev = newNode;
+            }
+        }
+        size++;
+    }
+
+    public int size() {
+        return size;
+    }
+
+    public void clear() {
+        head = null;
+        size = 0;
+    }
+
+    public void print() {
+        if (!isEmpty()) {
+            Node tmp = head;
+            while (tmp != null) { 
+                System.out.print(tmp.data + "\t");
+                tmp = tmp.next;
+            }
+            System.out.println("\nberhasil diisi");
+        } else {
+            System.out.println("Linked Lists Kosong");
+        }
+    }
+}
---
+++ src/Algoritma_dan_Struktur_Data/doublelinkedlist/Node.java
@@ -0,0 +1,12 @@
+package Algoritma_dan_Struktur_Data.doublelinkedlist;
+
+public class Node {
+    int data;
+    Node prev, next;
+
+    public Node(int data, Node prev, Node next) {
+        this.data = data;
+        this.prev = prev;
+        this.next = next;
+    }
+}
