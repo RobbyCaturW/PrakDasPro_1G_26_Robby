beaeb01dde93ec868b4099466e91725c0c316be5
RobbyCaturW <robbycatur330@gmail.com>
2025-06-05 09:47:20 +0700

    Praktikum 1

---
+++ src/Pertemuan14/Node25.java
@@ -0,0 +1,16 @@
+package Pertemuan14;
+
+public class Node25 {
+    Mahasiswa25 mahasiswa;
+    Node25 left, right;
+
+    public Node25() {
+    }
+
+    public Node25(Mahasiswa25 mahasiswa) {
+        this.mahasiswa = mahasiswa;
+        left = right = null;
+    }
+
+    
+}
