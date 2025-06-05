6612b8d0e95821229eda6fdddd3df4dd66fb2ccc
RobbyCaturW <robbycatur330@gmail.com>
2025-06-05 10:52:41 +0700

    Praktikum  2

---
+++ src/Algoritma_dan_Struktur_Data/Pertemuan14/BinaryTreeArray25.java
@@ -0,0 +1,25 @@
+package Algoritma_dan_Struktur_Data.Pertemuan14;
+
+public class BinaryTreeArray25 {
+    Mahasiswa25[] dataMahasiswa;
+    int idxLast;
+
+    public BinaryTreeArray25() {
+        this.dataMahasiswa = new Mahasiswa25[10];
+    }
+
+    void populateData (Mahasiswa25 dataMhs[], int idxLast) {
+        this.dataMahasiswa = dataMhs;
+        this.idxLast = idxLast;
+    }
+
+    void traverseInOrder(int idxStart) {
+        if (idxStart <= idxLast) {
+            if (dataMahasiswa[idxStart] != null) {
+                traverseInOrder(2*idxStart+1);
+                dataMahasiswa[idxStart].tampilInformasi();
+                traverseInOrder(2*idxStart+2);
+            }
+        }
+    }
+}
--- src/Algoritma_dan_Struktur_Data/Pertemuan14/Node25.java
+++ src/Algoritma_dan_Struktur_Data/Pertemuan14/Node25.java
@@ -1,4 +1,4 @@
-package Pertemuan14;
+package Algoritma_dan_Struktur_Data.Pertemuan14;
 
 public class Node25 {
     Mahasiswa25 mahasiswa;
